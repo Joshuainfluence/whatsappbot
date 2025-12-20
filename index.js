6b074ff2619d8fa19f0744fcb97992f13d8dadfa
joshuainfluence <joshuajulius2030@gmail.com>
2025-12-20 14:13:02 +0100

    new command

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+    console.log('Scan the QR code above with your WhatsApp');
+});
+
+client.on('ready', () => {
+    console.log('WhatsApp bot is ready!');
+});
+
+client.on('message', message => {
+    if (message.body.toLowerCase() === 'hi') {
+        message.reply('Hello! Connected to INFLUENCE bot🤖');
+    } else if (message.body.toLowerCase().includes('help')) {
+        message.reply('Available commands: hi, help');
+    }
+     else if (message.body.toLowerCase().includes('website')) {
+        message.reply('What kind of website do you need');
+    }
+});
+
+client.initialize();
